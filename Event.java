1f74810614d1d63afe1a027c7e56c6062ff4fd7d
William Hubert <wcolinhubert94@gmail.com>
2018-11-01 15:39:05 -0500

    Create Event.java

---
+++ Event.java
@@ -0,0 +1,16 @@
+package eventScheduler;
+
+public class Event {
+
+	double arrivalTime;
+	double serviceTime;
+	double completionTime;
+//	float responseRatio;
+//	float waitingTime;
+//	int type;
+//	boolean completed;
+//	int place;
+	// type 0 is arrival
+	//type 1 is departure
+
+}	
