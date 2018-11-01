a8921037d19a6a46066128184a8bd19936299661
William Hubert <wcolinhubert94@gmail.com>
2018-11-01 18:22:10 -0500

    Update HRRNext.java

--- HRRNext.java
+++ HRRNext.java
@@ -14,7 +14,7 @@ public class HRRNext {
 	public static double lambda;
 	public static int eventsCompleted;
 	public static double turnaroundArr[];
-	
+	public static double totalEventsInQueue;
 	
 	public static void init(){
 		list = new LinkedList<EventHRRN>();
@@ -22,66 +22,45 @@ public class HRRNext {
 		Event head = null;
 		newArrivalTime = 0;
 		serverBusy = false;
-		averageServiceTime = (double) 0.06;
+		averageServiceTime = (double) 0.042;
 		eventsCompleted = 0;
-		turnaroundArr = new double [501];
+		turnaroundArr = new double [10000];
 		generateEvents();
+		totalEventsInQueue = 0;
 	}
-	
 	
 	public static double poissonRandomArrival(double lambda) {
 		return (Math.log(Math.random())/-lambda);
 	}
 	
 	public static double getNumberOfEventsInQueue() {
-		int num = (readyQueue.size()-1);
-		return num;
-	}
-	
-	
-/*	
-	public static double getNumberOfEventsInQueue() {
-		int num = 0;
-		for (int i = 0; i < list.size(); i++) {
+		int num = (readyQueue.size());
+		System.out.println(num + " events in Ready Q");
+		for(int i=0; i < readyQueue.size();i++) {
 			EventHRRN e = new EventHRRN();
-			e = ((LinkedList<EventHRRN>) list).get(i);
-			if(e.arrivalTime <= currentTime && e.completed != true) {
-				num++;
-			}
+			e = ((LinkedList<EventHRRN>) readyQueue).get(i);
+			System.out.println("CurrentTime: " + currentTime);
+			System.out.println("Arr: " + e.arrivalTime + " Ser: " + e.serviceTime + " Res" + e.responseRatio);
 		}
 		return num;
 	}
-*/	
-	
-	public static double generateArrivalTime(double lambda) {
-	    Random r = new Random();
-	    double L = Math.exp(-lambda);
-	    float k = 0;
-	    double p = 1.0;
-	    do {
-	        p = p * r.nextDouble();
-	        k++;
-	    } while (p > L);
-	    return k - 1;
-	}
-	
-	
+
 	public static double generateServiceTime(double lambda) {
 		double expRandom = (double) (-lambda * Math.log(Math.random())/Math.log(2));
 		return expRandom;
 	}
+
 	
 	public static void generateReadyQueue() {
 		int i = 0;
 		EventHRRN e = new EventHRRN();
-		//e = ((LinkedList<EventHRRN>) list).get(i);
-		while(e.arrivalTime <= currentTime && !list.isEmpty()) {
+		e = ((LinkedList<EventHRRN>) list).get(0);
+		while(e.arrivalTime <= currentTime) {
 			e = ((LinkedList<EventHRRN>) list).get(i);
 			
 			if(!readyQueue.contains(e)) {
 				readyQueue.add(e);
 				list.remove(e);
-
 			}
 			i++;
 			try {
@@ -92,8 +71,11 @@ public class HRRNext {
 				break;
 			}		
 		}
+		if(readyQueue.isEmpty()) {
+			readyQueue.add(list.peek());
+			list.remove();
+		}
 	}
-	
 	
 	public static int findHighestR() {
 		generateReadyQueue();
@@ -113,41 +95,12 @@ public class HRRNext {
 		return i;
 	}
 	
-	
-	
-/*	
-	public static int findHighestR() {
-		int i = 0;
-		EventHRRN highestR = ((LinkedList<EventHRRN>) list).get(0);
-		highestR.waitingTime = (currentTime - highestR.arrivalTime); 
-		highestR.responseRatio = (highestR.serviceTime + highestR.waitingTime) / highestR.serviceTime;
-		for(int j =1; j < list.size()-1; j++) {
-			EventHRRN eve = ((LinkedList<EventHRRN>) list).get(j);
-			eve.waitingTime = (currentTime - eve.arrivalTime);
-			eve.responseRatio = (eve.serviceTime + eve.waitingTime) / eve.serviceTime;
-			if (eve.responseRatio > highestR.responseRatio && eve.arrivalTime <= currentTime) {
-				highestR = eve;
-				i = j;
-			}
-		}
-		return i;
-	}
-*/
-	
 	public static void runSim() {
-		//used to debug
-		int i = 0;
-		double totalEventsInQueue = 0;
-		while(eventsCompleted < 500) {
+		totalEventsInQueue = 0;
+		while(eventsCompleted < 10000) {
 			processEvent();
 			totalEventsInQueue += getNumberOfEventsInQueue();
-			
-			i++;
 		}
-		System.out.println("Befre dividing: " + totalEventsInQueue);
-		double averageNumOfEventsInQueue = (double)(totalEventsInQueue / 500);
-		System.out.println("");
-		System.out.println("The average Number of Events In Queue: " + averageNumOfEventsInQueue);
 		head = null;
 	}
 	
@@ -160,36 +113,14 @@ public class HRRNext {
 		e.completionTime = (currentTime + e.serviceTime);
 		currentTime = e.completionTime;
 		e.completed = true;
-		System.out.println("Event " + e.place + " ARRIVED: " + e.arrivalTime + " FINISHED: " + currentTime + " R: " + e.responseRatio);
+		//System.out.println("Event " + e.place + " ARRIVED: " + e.arrivalTime + " FINISHED: " + currentTime + " R: " + e.responseRatio);
 		turnaroundArr[eventsCompleted] = (e.completionTime - e.arrivalTime);
 		((LinkedList<EventHRRN>) readyQueue).remove(index);
-		
 		eventsCompleted++;
 	}
-	
-	
-	
-/*	
-	public static void processEvent() {
-		int index = findHighestR();
-		EventHRRN e = ((LinkedList<EventHRRN>) list).get(index);
-		if(currentTime < e.arrivalTime) {
-			currentTime = e.arrivalTime;
-		}
-		e.completionTime = (currentTime + e.serviceTime);
-		currentTime = e.completionTime;
-		e.completed = true;
-		System.out.println("Event " + e.place + " Arrived at time: " + e.arrivalTime + " and is finished at time: " + currentTime);
-		turnaroundArr[eventsCompleted] = (e.completionTime - e.arrivalTime);
-		((LinkedList<EventHRRN>) list).remove(index);
-		
-		eventsCompleted++;
-	}
-*/	
 	
 	public static void generateEvents() {
 		EventHRRN first = new EventHRRN();
-		//first.arrivalTime = generateArrivalTime(lambda);
 		first.arrivalTime = poissonRandomArrival(lambda);
 		first.place = 0;
 		newArrivalTime += first.arrivalTime;
@@ -202,19 +133,15 @@ public class HRRNext {
 		turnaroundArr[eventsCompleted] = (first.completionTime - first.arrivalTime);
 		list.remove();
 		eventsCompleted++;
-		
-		
-		for(int i = 1; i < 600; i++) {
+		for(int i = 1; i < 15000; i++) {
 			EventHRRN e = new EventHRRN();
-			//newArrivalTime += generateArrivalTime(lambda);
 			newArrivalTime += poissonRandomArrival(lambda);
 			e.place = i;
 			e.arrivalTime = newArrivalTime;
 			e.serviceTime = generateServiceTime(averageServiceTime);
 			e.completed = false;
-			System.out.println("Creating Process " + i + " Arrival Time: " + e.arrivalTime + " Service Time: " + e.serviceTime);
+			//System.out.println("Creating Process " + i + " Arrival Time: " + e.arrivalTime + " Service Time: " + e.serviceTime);
 			list.add(e);
-			
 		}
 	}
 	
@@ -230,23 +157,19 @@ public class HRRNext {
 		return (averageTurnaroundTime - averageServiceTime);
 	}
 	
-	
-	
 	public static void generateReport() {
 		double total = 0;
-		for(int j = 0; j < 501; j++) {
+		for(int j = 0; j < 10000; j++) {
 			total += turnaroundArr[j];
 		}
 
-		
+		double averageNumOfEventsInQueue = (double)(totalEventsInQueue / 5000);
 		double row = calculateUtilization(lambda);
-		double averageTurnaroundRate = (total / 500);
+		double averageTurnaroundRate = (total / 10000);
 		double averageWaitingTime = calculateAverageWaitingTime(averageTurnaroundRate);
 		double q = calculateQ(lambda, averageTurnaroundRate);
-		double totalThroughput = 500 / currentTime;
-		
+		double totalThroughput = 10000 / currentTime;
 		double testRow = (lambda / (1/ averageServiceTime));
-		//float testQ = (row / (1 - row));
 		
 		System.out.print("\n");
 		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
@@ -254,15 +177,10 @@ public class HRRNext {
 		System.out.println("CPU UTILIZATION: " + row);
 		System.out.println("TESTING ROW: " + testRow);
 		System.out.println("AVERAGE NUMBER OF PROCESSES IN QUEUE: " + q);
+		System.out.println("AVERAGE NUMBER OF EVENTS IN QUEUE: " + averageNumOfEventsInQueue);
 		System.out.println("TOTAL THROUGHPUT: " + totalThroughput);
-		//System.out.println("THIS NUM SHOULD MATCH Q: " + testQ);
 		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-	}
-	
-	
-	
-	
-	
+	}	
 	
 	public static void main(String args[])
 	{
@@ -279,27 +197,16 @@ public class HRRNext {
 			total += e.serviceTime;
 		}
 		
-		double avg = total /600;
-		System.out.println("Avg Service TIME: " + avg);
+		double avg = total /15000;
 		
 		
 		runSim();
 		generateReport();
-			
+		System.out.println("Avg Service TIME: " + avg);
+		System.out.println("Ready Queue finished size: " + readyQueue.size());
+		System.out.println("List finished size: " + list.size());
+
 		serverBusy = false;
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
