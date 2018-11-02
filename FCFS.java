c26577768c6e1851c68f3d88ee657d68c65fd250
William Hubert <wcolinhubert94@gmail.com>
2018-11-02 17:16:31 -0500

    Update FCFS.java

--- FCFS.java
+++ FCFS.java
@@ -7,184 +7,220 @@ import java.util.Queue;
  
 public class FCFS {
 	
-	public static Queue<Event> q;
-	public static int numberOfProccessesCompleted;
+	public static Queue<Event> list;
+	public static Queue<Event> readyQueue;
 	public static Event head;
-	public static float newCompletionTime;
-	public static float newArrivalTime;
-	public static float currentTime;
+	public static double newArrivalTime;
+	public static double currentTime;
 	public static boolean serverBusy;
+	public static double averageServiceTime;
+	public static double lambda;
+	public static int eventsCompleted;
+	public static double turnaroundArr[];
+	public static int cycles;
+	public static int totalEventsInQueue;
+	public static double timeSpentIdle;
 	
 	
 	public static void init(){
-		q = new LinkedList<Event>();
-		numberOfProccessesCompleted = 0;
-		Event head = null;
-		newCompletionTime = 0;
+		list = new LinkedList<Event>();
+		readyQueue = new LinkedList<Event>();
 		newArrivalTime = 0;
 		serverBusy = false;
 		currentTime = 0;
+		averageServiceTime = (double) 0.042;
+		eventsCompleted = 0;
+		turnaroundArr = new double [10000];
+		cycles = 0;
+		totalEventsInQueue = 0;
+		timeSpentIdle = 0;
+		generateEvents();
 	}
 	
-	
-	public static int generateArrivalTime(float lambda) {
-	    Random r = new Random();
-	    double L = Math.exp(-lambda);
-	    int k = 0;
-	    double p = 1.0;
-	    do {
-	        p = p * r.nextDouble();
-	        k++;
-	    } while (p > L);
-	    return k - 1;
+	public static double poissonRandomArrival(double lambda) {
+		return (Math.log(Math.random())/-lambda);
 	}
 	
-	
-	public static float generateServiceTime(float lambda) {
-		float expRandom = (float) (-lambda * Math.log(Math.random())/Math.log(2));
+	public static double generateServiceTime(double lambda) {
+		double expRandom = (double) (-lambda * Math.log(Math.random())/Math.log(2));
 		return expRandom;
 	}
 	
+	public static double calculateUtilization(double lambda) {
+		return (lambda * averageServiceTime);
+	}
+	
+	public static double calculateQ(double lambda, double Tq) {
+		return (lambda * Tq);
+	}
+	
+	public static double calculateAverageWaitingTime(double averageTurnaroundTime) {
+		return (averageTurnaroundTime - averageServiceTime);
+	}
+	
+	public static double getNumberOfEventsInQueue() {
+		return (readyQueue.size());
+
+	}
+	
+	public static void generateReadyQueue() {
+		Event e = new Event();
+		e = list.peek();
+		while(e.arrivalTime <= currentTime) {
+			e = list.peek();
+			if(!readyQueue.contains(e)) {
+				readyQueue.add(e);
+				list.remove();
+			}
+			try {
+				e = list.peek();
+			}
+			catch(IndexOutOfBoundsException ex) {
+				System.out.println("Index out of bounds exception...");
+				break;
+			}		
+		}
+		if(readyQueue.isEmpty()) {
+			readyQueue.add(list.peek());
+			list.remove();
+		}
+	}
+	
+	public static void processEvent() {
+		generateReadyQueue();
+		Event e = readyQueue.peek();
+		if(currentTime < e.arrivalTime) {
+			timeSpentIdle += (e.arrivalTime - currentTime);
+			currentTime = e.arrivalTime;
+		}
+		e.completionTime = (currentTime + e.serviceTime);
+		currentTime = e.completionTime;
+		e.completed = true;
+		turnaroundArr[eventsCompleted] = (e.completionTime - e.arrivalTime);
+		readyQueue.remove();
+		eventsCompleted++;
+	}
+	
+	public static void generateEvents() {
+		Event first = new Event();
+		first.arrivalTime = poissonRandomArrival(lambda);
+		newArrivalTime += first.arrivalTime;
+		first.serviceTime = generateServiceTime(averageServiceTime);
+		first.place = 0;
+		list.add(first);
+		readyQueue.add(first);
+		list.remove();
+		first.completionTime = (first.arrivalTime + first.serviceTime);
+		currentTime = first.completionTime;
+		first.completed = true;
+		turnaroundArr[eventsCompleted] = (first.completionTime - first.arrivalTime);
+		readyQueue.remove();
+		eventsCompleted++;
+		for(int i = 1; i < 20000; i++) {
+			Event e = new Event();
+			newArrivalTime += poissonRandomArrival(lambda);
+			e.arrivalTime = newArrivalTime;
+			e.serviceTime = generateServiceTime(averageServiceTime);
+			e.completed = false;
+			e.place = i;
+			list.add(e);
+		}
+	}
+	
+	public static void runSim() {
+		while(eventsCompleted < 10000) {
+			processEvent();
+			totalEventsInQueue += getNumberOfEventsInQueue();
+			cycles++;
+		}
+		head = null;
+	}
+
+	//used to generate the report with the requested metrics and display them to the console
+	public static void generateReport() {
+		double total = 0;
+		for(int j = 0; j < 10000; j++) {
+			total += turnaroundArr[j];
+		}
+
+		double averageNumOfEventsInQueue = (double)(totalEventsInQueue / cycles);
+		double averageTurnaroundRate = (total / 10000);
+		double averageWaitingTime = calculateAverageWaitingTime(averageTurnaroundRate);
+		double q = calculateQ(lambda, averageTurnaroundRate);
+		double totalThroughput = 10000 / currentTime;
+		double testRow = (lambda / (1/ averageServiceTime));
+		double row = (1 - (timeSpentIdle / currentTime));
+		double testQ = ((row) / (1 - row));
+		
+		System.out.print("\n");
+		System.out.println("* * * * * * * * *ACTUAL DATA * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("AVERAGE TURNAROUND TIME: " + averageTurnaroundRate);
+		System.out.println("TOTAL THROUGHPUT: " + totalThroughput);
+		System.out.println("CPU UTILIZATION: " + row);
+		System.out.println("AVERAGE NUMBER OF PROCESSES IN READY QUEUE: " + averageNumOfEventsInQueue);
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.print("\n");
+		System.out.println("* * * * * * * * * TEST DATA* * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("TESTING ROW: " + testRow);
+		System.out.println("AVERAGE NUMBER OF PROCESSES IN QUEUE: " + q);
+		System.out.println("TEST Q WITH REAL ROW: " + testQ);
+		System.out.println("AVERAGE WAITING TIME: " + averageWaitingTime);
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+	}
+	
+	
 /*	
-	public static float urand() {
-		Random rand = new Random();
-		float  n = rand.nextInt(1) + 0;
-		return n;
+	public static void generateReport() {
+		double total = 0;
+		for(int j = 0; j < 10000; j++) {
+			total += turnaroundArr[j];
+		}
+
+		
+		double row = calculateUtilization(lambda);
+		double averageTurnaroundRate = (total / 10000);
+		double averageWaitingTime = calculateAverageWaitingTime(averageTurnaroundRate);
+		double q = calculateQ(lambda, averageTurnaroundRate);
+		double totalThroughput = 10000 / currentTime;
+		
+		double testRow = (lambda / (1/ averageServiceTime));
+		//float testQ = (row / (1 - row));
+		
+		System.out.print("\n");
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("AVERAGE TURNAROUND TIME: " + averageTurnaroundRate);
+		System.out.println("CPU UTILIZATION: " + row);
+		System.out.println("TESTING ROW: " + testRow);
+		System.out.println("AVERAGE NUMBER OF PROCESSES IN QUEUE: " + q);
+		System.out.println("TOTAL THROUGHPUT: " + totalThroughput);
+		//System.out.println("THIS NUM SHOULD MATCH Q: " + testQ);
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
 	}
-*/		
-	
-	public static void scheduleEvent(int i, float time, Event e) {
-		if(i == 0) {
-			q.add(e);
-			System.out.println("Process arrived to Queue at time " + time);
-			System.out.println("With a service time of " + e.serviceTime);
-			
-		}
-		else {
-			currentTime = time;
-			q.remove();
-			System.out.println("Process departed from Queue at " + currentTime);
-		}
-	}
-	
-	
-	public static void processEventArrival(Event e) {
-		if(serverBusy == false) {
-			serverBusy = true;
-			scheduleEvent(0, e.arrivalTime, e);
-			scheduleEvent(1, (currentTime + e.serviceTime), e);
-		}
-		else {
-			scheduleEvent(0, currentTime, e);
-			scheduleEvent(1, (currentTime + e.serviceTime), e);
-		}
-	}
-	
+*/
 	
 	
 	public static void main(String args[])
 	{
-		init();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter lambda(average arrival rate): ");
-		float lambda = sc.nextFloat();
+		lambda = sc.nextDouble();
+		init();
 		
-		System.out.println("enter no of process: ");
-		int n = sc.nextInt();
-		
-/*		
-		for(int i =0; i <= n; i++) {
-			Event e = new Event(0, currentTime, lambda);
-			processEventArrival(e);
-		}
-*/		
-
-/*		
-		Event ae = new Event(0, currentTime, lambda);
-		q.add(ae);
-		Event head = q.peek();
-		System.out.println("The first Arrival Time is: " + ae.arrivalTime);
-		System.out.println("The first Service Time is: " + ae.serviceTime);
-		ae.completionTime = (ae.arrivalTime + ae.serviceTime);
-		currentTime = ae.completionTime;
-		System.out.println("The first process completes at " + ae.completionTime );
-		
-		Event nextArrival = new Event(0, currentTime, lambda);
-		if(nextArrival.arrivalTime <= )
-		q.remove();
-*/
-		
-
-		for(int i = 0; i <= n; i++)
-		{
-			
-			Event ae = new Event();
-			
-			newArrivalTime += generateArrivalTime(lambda);
-			ae.arrivalTime = newArrivalTime;
-			
-			ae.serviceTime = generateServiceTime(lambda);
-			
-			System.out.println("The " + (i+1) + " Arrival Time is: " + ae.arrivalTime);
-			System.out.println("The " + (i+1) + " Service Time is: " + ae.serviceTime);
-
-			q.add(ae);			
-			
-				
-		}	
-
-		
-		float turnaroundArr[] = new float[15];
-		
-		int i = 0;
-		float time = 0;
-
-		while(numberOfProccessesCompleted < 11) {
-			
-			if(!serverBusy) {
-				serverBusy = true;
-				head = q.peek();
-				time = (head.arrivalTime + head.serviceTime);
-				head.completionTime = time;
-			}
-			else {
-				head = q.peek();
-				if(head.arrivalTime < time) {
-					time += head.serviceTime;
-					System.out.println("Process arived before CPU was ready");
-					head.completionTime = time;
-				}
-				else {
-					time = head.arrivalTime;
-					time += head.serviceTime;
-					head.completionTime = time;
-					System.out.println("Process arrived after CPU was ready, CPU was idle for a bit.");
-				}
-			}
-			turnaroundArr[i] = (head.completionTime - head.arrivalTime);
-			System.out.println("Process " + (i+1) + " ARRIVAL: " + head.arrivalTime + 
-					" SERVICE: " + head.serviceTime + " COMPLETION: " + head.completionTime);
-			i++;
-			q.remove();
-			numberOfProccessesCompleted++;
+		double total = 0;
+		for(int i = 0; i < list.size();i++) {
+			Event e = new Event();
+			e = ((LinkedList<Event>) list).get(i);
+			total += e.serviceTime;
 		}
 		
+		double avg = total /15000;
+		
+		runSim();
+		generateReport();
+		
+		System.out.println("Average Service Time: " + avg);
+			
 		serverBusy = false;
 		
-		float total = 0;
-		for(int j = 0; j < 10; j++) {
-			total += turnaroundArr[j];
-		}
-
-		float averageTurnaroundRate = (total / 10);
-		System.out.println("Average Turnaround Rate: " + averageTurnaroundRate);
-		
-/*		
-			q.remove();
-			q.remove();
-			head = q.peek();
-			
-			System.out.println("THE HEAD OF THE QUEUE AFTER 2 DELETIONS HAS ARRIVAL TIME: " + head.arrivalTime + "AND A SERVICE TIME OF: " + head.serviceTime );
-*/		
 	}
 }
