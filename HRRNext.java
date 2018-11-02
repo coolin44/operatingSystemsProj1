b106a92002304f188247e78735c1e651ac22516a
William Hubert <wcolinhubert94@gmail.com>
2018-11-02 17:15:44 -0500

    Update HRRNext.java

--- HRRNext.java
+++ HRRNext.java
@@ -3,23 +3,35 @@ package eventScheduler;
 import java.util.*; 
 
 public class HRRNext {
-	
+	//used to hold generated events
 	public static Queue<EventHRRN> list;
+	//used to hold events that are ready to be processed
 	public static Queue<EventHRRN> readyQueue;
-	public static Event head;
+	//used to generate random arrival times in an increading order
 	public static double newArrivalTime;
+	//used to hold the current time
 	public static double currentTime;
+	//used to flag if the server is busy or not
 	public static boolean serverBusy;
+	//used to calculate the average service time
 	public static double averageServiceTime;
+	//used to hold the user input lambda(1<= x <= 30)
 	public static double lambda;
+	//used to hold the number of events completed
 	public static int eventsCompleted;
+	//used to calculate the average turnaround time
 	public static double turnaroundArr[];
+	//used to add all of the events in Ready Queue 
 	public static double totalEventsInQueue;
+	//used to calculate the number of times the 'getNumberOfEventsInQueue()' method is called
+	public static int cycles;
+	//used to hold the amount of time the CPU is idle
+	public static double timeSpentIdle;
 	
+	//this method is used to initialize all global variables and generate the list of events
 	public static void init(){
 		list = new LinkedList<EventHRRN>();
 		readyQueue = new LinkedList<EventHRRN>();
-		Event head = null;
 		newArrivalTime = 0;
 		serverBusy = false;
 		averageServiceTime = (double) 0.042;
@@ -27,37 +39,34 @@ public class HRRNext {
 		turnaroundArr = new double [10000];
 		generateEvents();
 		totalEventsInQueue = 0;
+		cycles = 0;
+		timeSpentIdle = 0;
 	}
 	
+	//this method is used to get a random arrival time that follows a poisson distribution
 	public static double poissonRandomArrival(double lambda) {
 		return (Math.log(Math.random())/-lambda);
 	}
 	
+	//used to retrieve the amount of events in the Ready Queue
 	public static double getNumberOfEventsInQueue() {
-		int num = (readyQueue.size());
-		System.out.println(num + " events in Ready Q");
-		for(int i=0; i < readyQueue.size();i++) {
-			EventHRRN e = new EventHRRN();
-			e = ((LinkedList<EventHRRN>) readyQueue).get(i);
-			System.out.println("CurrentTime: " + currentTime);
-			System.out.println("Arr: " + e.arrivalTime + " Ser: " + e.serviceTime + " Res" + e.responseRatio);
-		}
-		return num;
+		return (readyQueue.size());
+
 	}
 
+	//used to generate a random service time that follows an exponential distribution with an average service time being .06
 	public static double generateServiceTime(double lambda) {
 		double expRandom = (double) (-lambda * Math.log(Math.random())/Math.log(2));
 		return expRandom;
 	}
 
-	
+	//used to find all events that are ready and put them in the Ready Queue
 	public static void generateReadyQueue() {
 		int i = 0;
 		EventHRRN e = new EventHRRN();
 		e = ((LinkedList<EventHRRN>) list).get(0);
 		while(e.arrivalTime <= currentTime) {
 			e = ((LinkedList<EventHRRN>) list).get(i);
-			
 			if(!readyQueue.contains(e)) {
 				readyQueue.add(e);
 				list.remove(e);
@@ -77,6 +86,7 @@ public class HRRNext {
 		}
 	}
 	
+	//used to find the event that has the highest response ratio
 	public static int findHighestR() {
 		generateReadyQueue();
 		int i = 0;
@@ -95,30 +105,33 @@ public class HRRNext {
 		return i;
 	}
 	
+	//used to run the simulator
 	public static void runSim() {
 		totalEventsInQueue = 0;
 		while(eventsCompleted < 10000) {
 			processEvent();
 			totalEventsInQueue += getNumberOfEventsInQueue();
+			cycles++;
 		}
-		head = null;
 	}
 	
+	//used to process the event that currently has the highest response ratio
 	public static void processEvent() {
 		int index = findHighestR();
 		EventHRRN e = ((LinkedList<EventHRRN>) readyQueue).get(index);
 		if(currentTime < e.arrivalTime) {
+			timeSpentIdle += (e.arrivalTime - currentTime);
 			currentTime = e.arrivalTime;
 		}
 		e.completionTime = (currentTime + e.serviceTime);
 		currentTime = e.completionTime;
 		e.completed = true;
-		//System.out.println("Event " + e.place + " ARRIVED: " + e.arrivalTime + " FINISHED: " + currentTime + " R: " + e.responseRatio);
 		turnaroundArr[eventsCompleted] = (e.completionTime - e.arrivalTime);
 		((LinkedList<EventHRRN>) readyQueue).remove(index);
 		eventsCompleted++;
 	}
 	
+	//used to generate a list of 15000 events
 	public static void generateEvents() {
 		EventHRRN first = new EventHRRN();
 		first.arrivalTime = poissonRandomArrival(lambda);
@@ -140,48 +153,58 @@ public class HRRNext {
 			e.arrivalTime = newArrivalTime;
 			e.serviceTime = generateServiceTime(averageServiceTime);
 			e.completed = false;
-			//System.out.println("Creating Process " + i + " Arrival Time: " + e.arrivalTime + " Service Time: " + e.serviceTime);
 			list.add(e);
 		}
 	}
 	
+	//used to calculate test data to compare with the real data found
 	public static double calculateUtilization(double lambda) {
 		return (lambda * averageServiceTime);
 	}
 	
+	//used to calculate test data to compare with the real data found
 	public static double calculateQ(double lambda, double Tq) {
 		return (lambda * Tq);
 	}
 	
+	//used to calculate test data to compare with the real data found
 	public static double calculateAverageWaitingTime(double averageTurnaroundTime) {
 		return (averageTurnaroundTime - averageServiceTime);
 	}
-	
+
+	//used to generate the report with the requested metrics and display them to the console
 	public static void generateReport() {
 		double total = 0;
 		for(int j = 0; j < 10000; j++) {
 			total += turnaroundArr[j];
 		}
 
-		double averageNumOfEventsInQueue = (double)(totalEventsInQueue / 5000);
-		double row = calculateUtilization(lambda);
+		double averageNumOfEventsInQueue = (double)(totalEventsInQueue / cycles);
 		double averageTurnaroundRate = (total / 10000);
 		double averageWaitingTime = calculateAverageWaitingTime(averageTurnaroundRate);
 		double q = calculateQ(lambda, averageTurnaroundRate);
 		double totalThroughput = 10000 / currentTime;
 		double testRow = (lambda / (1/ averageServiceTime));
+		double row = (1 - (timeSpentIdle / currentTime));
+		double testQ = ((row) / (1 - row));
 		
 		System.out.print("\n");
-		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.println("* * * * * * * * *ACTUAL DATA * * * * * * * * * * * * * * * * * * * * * * * *");
 		System.out.println("AVERAGE TURNAROUND TIME: " + averageTurnaroundRate);
+		System.out.println("TOTAL THROUGHPUT: " + totalThroughput);
 		System.out.println("CPU UTILIZATION: " + row);
+		System.out.println("AVERAGE NUMBER OF PROCESSES IN READY QUEUE: " + averageNumOfEventsInQueue);
+		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+		System.out.print("\n");
+		System.out.println("* * * * * * * * * TEST DATA* * * * * * * * * * * * * * * * * * * * * * * *");
 		System.out.println("TESTING ROW: " + testRow);
 		System.out.println("AVERAGE NUMBER OF PROCESSES IN QUEUE: " + q);
-		System.out.println("AVERAGE NUMBER OF EVENTS IN QUEUE: " + averageNumOfEventsInQueue);
-		System.out.println("TOTAL THROUGHPUT: " + totalThroughput);
+		System.out.println("TEST Q WITH REAL ROW: " + testQ);
+		System.out.println("AVERAGE WAITING TIME: " + averageWaitingTime);
 		System.out.println("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
 	}	
 	
+	//main function
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
@@ -203,10 +226,9 @@ public class HRRNext {
 		runSim();
 		generateReport();
 		System.out.println("Avg Service TIME: " + avg);
-		System.out.println("Ready Queue finished size: " + readyQueue.size());
-		System.out.println("List finished size: " + list.size());
 
 		serverBusy = false;
 		
 	}
 }
+	
